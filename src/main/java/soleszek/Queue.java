f251b15a4155cafdc95a4f65c9425199283fd9b4
Sylwester Oleszek <sylwester.oleszek@gmail.com>
2018-07-22 15:47:34 +0200

    Ok

---
+++ src/main/java/soleszek/Queue.java
@@ -0,0 +1,11 @@
+package soleszek;
+
+public interface Queue<T> {
+    void push(T value);
+
+    T poll();
+
+    T peek();
+
+    boolean isEmpty();
+}
---
+++ src/main/java/soleszek/QueueImpl.java
@@ -0,0 +1,75 @@
+package soleszek;
+
+public class QueueImpl<T> implements Queue<T> {
+
+    private Item head;
+    private Item tail;
+
+    @Override
+    public void push(T item) {
+        Item newTail = new Item();
+        newTail.value = item;
+
+        if (head == null && tail == null) {
+            tail = newTail;
+            head = newTail;
+        } else if (head == tail) {
+            tail = newTail;
+            head.next = newTail;
+        } else {
+            tail.next = newTail;
+            tail = newTail;
+        }
+    }
+
+    @Override
+    public T poll() {
+        if (head == null) {
+            return null;
+        }
+        T value = head.value;
+        head = head.next;
+
+        if (head == null) {
+            tail = null;
+        }
+
+        return value;
+    }
+
+    @Override
+    public T peek() {
+
+        if (head == null) {
+            return null;
+        }
+
+        return head.value;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public String toString() {
+        return "QueueImpl{" +
+                "head=" + head +
+                ", tail=" + tail +
+                '}';
+    }
+
+    private class Item {
+        T value;
+        Item next;
+
+        @Override
+        public String toString() {
+            return "Item{" +
+                    "value=" + value +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
