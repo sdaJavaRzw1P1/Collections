ddade5ddbd217f9f07767f7684681bd6885293c6
Sylwester Oleszek <sylwester.oleszek@gmail.com>
2018-07-22 12:39:50 +0200

    Random

--- src/main/java/pl/sda/soleszek/ArrayRandom.java
+++ src/main/java/pl/sda/soleszek/ArrayRandom.java
@@ -1,8 +1,22 @@
 package pl.sda.soleszek;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class ArrayRandom {
     public static void main(String[] args) {
         int a = 5;
-        int b = 15;
+        int b = 8;
+        int n = 10;
+
+        int[] array = new int[n];
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(b - a + 1) + a;
+        }
+
+        System.out.println(Arrays.toString(array));
+
     }
 }
