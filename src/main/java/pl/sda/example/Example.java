1eeb9fc0ff2f07f144f61198fefebbc4ffef4f67
Jakub Pelczar <jakubpelczar92@gmail.com>
2018-07-22 08:58:19 +0200

    Init repo

---
+++ src/main/java/pl/sda/example/Example.java
@@ -0,0 +1,4 @@
+package pl.sda.example;
+
+public class Example {
+}
---
+++ src/test/java/pl/sda/example/ExampleTest.java
@@ -0,0 +1,17 @@
+package pl.sda.example;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class ExampleTest {
+
+    @Test
+    public void testExample() {
+        Assert.assertEquals(4, 2 + 2);
+    }
+
+}
