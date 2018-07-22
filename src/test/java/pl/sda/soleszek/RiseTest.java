b0200fa84705552e4d08f07590188ab61e138d76
Sylwester Oleszek <sylwester.oleszek@gmail.com>
2018-07-22 11:36:33 +0200

    .

---
+++ src/test/java/pl/sda/soleszek/RiseTest.java
@@ -0,0 +1,38 @@
+package pl.sda.soleszek;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RiseTest {
+
+    private final static int BIG_PACK_QUANTITY = 10;
+    private final static int SMALL_PACK_QUANTITY = 25;
+
+    private Rise rice;
+
+    @Before
+    public void setUp() throws Exception {
+        rice = new Rise(BIG_PACK_QUANTITY, SMALL_PACK_QUANTITY);
+    }
+
+    @Test
+    public void testPlaceOneOrder() {
+        assertTrue(rice.placeOrder(10));
+        System.out.println(rice.toString());
+    }
+
+    @Test
+    public void testPlaceOrdersAndRunOutRice() {
+        assertTrue(rice.placeOrder(10));
+        System.out.println("1: " + rice);
+        assertTrue(rice.placeOrder(29));
+        System.out.println("2: " + rice);
+        assertTrue(rice.placeOrder(36));
+        System.out.println("3: " + rice);
+        assertFalse(rice.placeOrder(1));
+        System.out.println("4: " + rice);
+    }
+
+}
